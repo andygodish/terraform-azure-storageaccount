733524a8d897fdea439f070eb75a6e1e03fb5191
Andy Godish <agodish18@gmail.com>
2024-03-16 10:01:45 -0600

    fix: added basic outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "sa_id" {
-  value       = azurerm_storage_account.sa.id
+  value       = azurerm_storage_account.this.id
   description = "The resource id of the storage account"
 }
 output "sa_name" {
-  value       = azurerm_storage_account.sa.name
+  value       = azurerm_storage_account.this.name
   description = "The name of the storage account resource"
 }
